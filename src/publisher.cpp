b17adef0bff5cdf684dc502d5fdecf5034815183
dhanushka <liyanagedc@gmail.com>
2026-03-01 20:18:10 +0200

    resize

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -138,7 +138,7 @@ void imagePublisher(){
     while(running){
 
         if ((frame_height > 0) && (frame_width > 0)){
-            scaleFactor =  double(IMAGE_WIDTH / frame_width);
+            scaleFactor =  double(IMAGE_WIDTH) / double(frame_width);
 
             resizedHeight = (frame_height * scaleFactor);
             resizedWidth = (frame_width * scaleFactor);
